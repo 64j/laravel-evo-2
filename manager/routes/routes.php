545ce7e5f37c51eb8432112284a321b86dae10b8
Serg <64j@mail.ru>
2022-10-25 13:50:13 +0300

    up

--- manager/routes/routes.php
+++ manager/routes/routes.php
@@ -6,15 +6,15 @@ Route::group([
     'middleware' => 'web',
     'namespace' => '\\Manager\\Http\\Controllers'
 ], function () {
-    Route::get('/manager', 'Home@index')->middleware('auth');
+    Route::get('/manager', 'Home@index')->middleware('auth')->name('home');
     Route::get('/manager/login', 'Auth@formLogin')->name('login');
     Route::post('/manager/login', 'Auth@login');
+    Route::get('/manager/api')->middleware('guest');
 });
 
 Route::group([
     'middleware' => 'api',
     'namespace' => '\\Manager\\Http\\Controllers'
 ], function () {
-    Route::get('/manager/api', 'Controller@handle')->middleware('guest');
     Route::post('/manager/api', 'Controller@handle')->middleware('auth:api');
 });
--- manager/src/Providers/RouteServiceProvider.php
+++ manager/src/Providers/RouteServiceProvider.php
@@ -19,7 +19,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/manager';
+    public const HOME = null;
 
     /**
      * The controller namespace for the application.
