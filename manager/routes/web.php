63f9ec28fb61606f909cfbbf936a432d32ef9eef
Serg <64j@mail.ru>
2022-10-25 07:41:53 +0300

    up

--- manager/routes/web.php
+++ manager/routes/web.php
@@ -3,7 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/manager', 'HomeController@index')->middleware(['auth']);
-Route::get('/login', 'AuthController@formLogin')->name('login');
-Route::post('/login', 'AuthController@login');
-Route::get('/manager/login', 'AuthController@formLogin');
+Route::get('/manager/login', 'AuthController@formLogin')->name('login');
 Route::post('/manager/login', 'AuthController@login');
