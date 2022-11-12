6a5392e62736956261aa3627352adef6f4a6f5e1
Serg <64j@mail.ru>
2022-11-12 18:42:52 +0300

    up

--- manager/resources/js/components/ActionsButtons.vue
+++ manager/resources/js/components/ActionsButtons.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="actions-buttons fixed rounded-b bg-gray-500/5 p-2 pl-1 right-0 z-50">
+  <div class="actions-buttons fixed shadow rounded-b bg-gray-500/5 p-2 pl-1 right-0 z-50">
 
     <button
         type="button"
@@ -19,7 +19,7 @@
 
     <button
         type="button"
-        class="inline-flex items-center bg-green-700 hover:bg-green-800 text-white text-sm font-bold py-1 px-2 rounded ml-1"
+        class="inline-flex items-center bg-green-600 hover:bg-green-700 text-white text-sm font-bold py-1 px-2 rounded ml-1"
         @click="$emit('action', 'save', $event)" :title="$root.lang('save')">
       <i class="fa fa-save fa-fw"/>
       <span class="ml-1">{{ $root.lang('save') }}</span>
