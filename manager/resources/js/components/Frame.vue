a096492b64d74329e37d6d55c4e9640518251c36
Serg <64j@mail.ru>
2022-11-20 22:17:28 +0300

    up

---
+++ manager/resources/js/components/Frame.vue
@@ -0,0 +1,16 @@
+<template>
+  <iframe :src="src" class="w-full h-full overflow-auto border-none"/>
+</template>
+
+<script>
+export default {
+  name: 'FrameComponent',
+  props: {
+    src: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
